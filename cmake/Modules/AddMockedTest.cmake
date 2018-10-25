47e783e1130453491b30e84d414b5928386d92b8
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2018-10-25 17:31:14 +0200

    Rewrite interface for add_mocked_test()
    Now it allows to pass every possible argument to underlying
    add_cmocka_test(), automating some repeating patterns, especially adding
    test source in form test_${name}.c, as this is how it should always be
    called. It also links CMocka library.
    
    Main new feature however is possibility to define list of functions that
    will be mocked in the test executable using MOCKS argument. This way
    caller does not have to repeat -Wl,--wrap= construct or write for loops
    to do it by himself.

--- cmake/Modules/AddMockedTest.cmake
+++ cmake/Modules/AddMockedTest.cmake
@@ -20,14 +20,38 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-## \function add_mocked_test
-#  \brief Add unit test with mocking support
+## Add unit test with mocking support
 #  \param name unit test name (excluding extension and 'test_' prefix)
-#  \param objects optional list of objects to include in test module
+#  \param SOURCES optional list of source files to include in test executable
+#  (beside test_${name}.c)
+#  \param MOCKS optional list of functions to be mocked in executable
+#  \param COMPILE_OPTIONS optional list of options for the compiler
+#  \param LINK_LIBRARIES optional list of libraries to link (used as
+#  -l${LINK_LIBRARIES})
+#  \param LINK_OPTIONS optional list of options to be passed to linker
 function(add_mocked_test name)
+  # parse arguments passed to the function
+  set(options )
+  set(oneValueArgs )
+  set(multiValueArgs SOURCES MOCKS COMPILE_OPTIONS LINK_LIBRARIES LINK_OPTIONS)
+  cmake_parse_arguments(ADD_MOCKED_TEST "${options}" "${oneValueArgs}"
+    "${multiValueArgs}" ${ARGN} )
+
+  # create link flags for mocks
+  set(link_flags "")
+  foreach (mock ${ADD_MOCKED_TEST_MOCKS})
+    set(link_flags "${link_flags} -Wl,--wrap=${mock}")
+  endforeach(mock)
+
+  # define test
   add_cmocka_test(test_${name}
-                  SOURCES test_${name}.c ${ARGN}
+                  SOURCES test_${name}.c ${ADD_MOCKED_TEST_SOURCES}
                   COMPILE_OPTIONS ${DEFAULT_C_COMPILE_FLAGS}
-                  LINK_LIBRARIES ${CMOCKA_LIBRARIES})
+                                  ${ADD_MOCKED_TEST_COMPILE_OPTIONS}
+                  LINK_LIBRARIES ${CMOCKA_LIBRARIES}
+                                 ${ADD_MOCKED_TEST_LINK_LIBRARIES}
+                  LINK_OPTIONS ${link_flags} ${ADD_MOCKED_TEST_LINK_OPTIONS})
+
+  # allow using includes from src/ directory
   target_include_directories(test_${name} PRIVATE ${CMAKE_SOURCE_DIR}/src)
 endfunction(add_mocked_test)
