7a731515a0dde5eeba4803f50faf0efaeae3b4d9
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2018-11-06 19:04:58 +0100

    Implement getopt
    Common use case of getopt function with support for short and long
    options; non-argument options, required arguments and optional
    arguments, options with long option only, etc.

--- src/program.c
+++ src/program.c
@@ -22,8 +22,12 @@
  * SOFTWARE.
  */
 #include <stdio.h>
+#include <limits.h>
+#include <getopt.h>
 
 #include "module.h"
+
+#define LONG_OPTION (( CHAR_MAX + 1 ))
 
 int internal(int a, int b)
 {
@@ -32,10 +36,95 @@ int internal(int a, int b)
 
 int main(int argc, char **argv)
 {
+  int c;
+  int digit_optind = 0;
+
   int one = 10;
   int two = 2;
   int result = internal(one, two);
   result += external(result);
   printf("%d\n", result);
+
+  while (1) {
+    int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+      /* {name, has_arg, flag, val} */
+      {"option", required_argument, 0, 'o' },
+      {"switch", no_argument, 0, 's' },
+      {"dual", optional_argument, 0, 'd' },
+      {"long", no_argument, 0, LONG_OPTION },
+      {0, 0, 0, 0 }
+    };
+
+    c = getopt_long(argc, argv, "o:sd::",
+        long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch (c) {
+      case 0:
+        /* for default long options
+         * (option->flag==0 -> 4th element of struct option) */
+        printf("option %s", long_options[option_index].name);
+        if (optarg)
+          printf(" with arg %s", optarg);
+        printf("\n");
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+        if (digit_optind != 0 && digit_optind != this_option_optind)
+          printf("digits occur in two different argv-elements.\n");
+        digit_optind = this_option_optind;
+        printf("option %c\n", c);
+        break;
+
+      case 'o':
+        /* --option=VAL */
+        printf("option with value '%s'\n", optarg);
+        break;
+
+      case 's':
+        /* --switch */
+        printf("switch\n");
+        break;
+
+      case 'd':
+        /* --dual[=VAL] */
+        if (optarg == NULL)
+        {
+        printf("dual without value\n");
+        }
+        else
+        {
+        printf("dual with value '%s'\n", optarg);
+        }
+        break;
+
+      case LONG_OPTION:
+        /* --long */
+        printf("long option\n");
+        break;
+
+      case '?':
+        /* -* not in optstring */
+        break;
+
+      default:
+        /* optstrings not handled (mistakes?) */
+        printf("?? getopt returned character code 0%o ??\n", c);
+    }
+  }
+
+  /* positional arguments */
+  if (optind < argc) {
+      printf("non-option ARGV-elements: ");
+      while (optind < argc)
+    printf("%s ", argv[optind++]);
+      printf("\n");
+  }
+
   return 0;
 }
